8c3be2a11244009051ba7e402ea8cbcef62f11bf
Gregory <gregory.torok@outlook.com>
2025-12-13 19:44:04 +0100

    Add Transaction Detail Page

--- app/(tabs)/transactions/[id].tsx
+++ app/(tabs)/transactions/[id].tsx
@@ -1,11 +1,124 @@
-import { Text, View } from "react-native";
+import { TransactionsContext } from "@/context/TransactionsContext";
+import { COLORS } from "@/constants/colors";
+import { useLocalSearchParams } from "expo-router";
+import { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 const TransactionDetail = () => {
+  const { id } = useLocalSearchParams<{ id: string }>();
+
+  const { transactions } = useContext(TransactionsContext);
+  const transaction = transactions.find(
+    (transaction) => transaction.id === Number(id)
+  );
+
+  if (!transaction) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.notFound}>Transaction not found</Text>
+      </View>
+    );
+  }
+
+  const isCredit = transaction?.credit !== undefined && transaction.credit! > 0;
+  const amount = isCredit ? transaction.credit! : transaction?.debit!;
+  const formattedAmount = amount.toFixed(2);
+  const formattedDate = new Date(
+    transaction!.transactionDate
+  ).toLocaleDateString("nl-BE", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  });
+
   return (
-    <View>
-      <Text>TransactionDetail</Text>
+    <View style={styles.container}>
+      <View
+        style={[
+          styles.card,
+          isCredit ? styles.creditBorder : styles.debitBorder,
+        ]}
+      >
+        <View style={styles.row}>
+          <Text style={styles.label}>Description: </Text>
+          <Text style={styles.value}>{transaction?.description}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Category: </Text>
+          <Text style={styles.value}>{transaction?.category}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Date: </Text>
+          <Text style={styles.value}>{formattedDate}</Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.label}>Amount: </Text>
+          <Text
+            style={[
+              styles.amount,
+              { color: isCredit ? COLORS.credit : COLORS.debit },
+            ]}
+          >
+            {isCredit ? "+" : "-"}€{formattedAmount}
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.light,
+  },
+  card: {
+    marginTop: 10,
+    alignSelf: "center",
+    width: "90%",
+    backgroundColor: COLORS.card,
+    borderRadius: 20,
+    padding: 24,
+    shadowColor: COLORS.dark,
+    shadowOffset: { width: 0, height: 6 },
+    shadowOpacity: 0.15,
+    shadowRadius: 10,
+    elevation: 6,
+  },
+  creditBorder: {
+    borderWidth: 2,
+    borderColor: COLORS.credit,
+  },
+  debitBorder: {
+    borderWidth: 2,
+    borderColor: COLORS.debit,
+  },
+  row: {
+    flexDirection: "column",
+    marginBottom: 12,
+    flexWrap: "wrap",
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: "400",
+    color: COLORS.dark,
+  },
+  value: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: COLORS.dark,
+    flexShrink: 1,
+  },
+  amount: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  notFound: {
+    marginTop: 10,
+    alignSelf: "center",
+    fontSize: 18,
+    color: COLORS.debit,
+  },
+});
 
 export default TransactionDetail;
